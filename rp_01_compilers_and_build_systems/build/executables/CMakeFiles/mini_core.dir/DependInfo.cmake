a16de14f7df328b7352990e69bbe2cf9f4156914
Antonio Carpes Martinez <antonioalbertocarpesmartinez@gmail.com>
2024-10-07 20:18:06 +0200

    Exercise CMake_files.txt

---
+++ rp_01_compilers_and_build_systems/build/executables/CMakeFiles/mini_core.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/antonio/Documents/EMAI SECOND YEAR/Robot programming/robotprogramming_2024_25/exercises/rp_01_compilers_and_build_systems/executables/many_object_in_stack_example.cpp" "executables/CMakeFiles/mini_core.dir/many_object_in_stack_example.cpp.o" "gcc" "executables/CMakeFiles/mini_core.dir/many_object_in_stack_example.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
